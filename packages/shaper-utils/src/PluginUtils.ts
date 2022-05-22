115300807798d5c1498276034f616cdaf93b6bba
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-05-22 12:45:36 -0400

    Added comments to PluginUtils.ts

--- packages/shaper-utils/src/PluginUtils.ts
+++ packages/shaper-utils/src/PluginUtils.ts
@@ -1,11 +1,21 @@
 import path from 'path';
-import { PackageJson, Plugin } from './models';
 import { JsonUtils } from './JsonUtils';
+import { PackageJson, Plugin } from './models';
 
+/**
+ * Returns shaper plugins installed in the package.json file
+ * of the current working directory
+ */
 function getDynamicPlugins(): Map<string, Plugin> {
   return getInstalledPluginsFromPackageJson(process.cwd());
 }
 
+/**
+ * Returns shaper plugins installed in the package.json file
+ * of the specified root path
+ *
+ * @param rootPath
+ */
 function getInstalledPluginsFromPackageJson(
   rootPath: string
 ): Map<string, Plugin> {
@@ -13,29 +23,30 @@ function getInstalledPluginsFromPackageJson(
     `${rootPath}/package.json`
   );
 
+  // Create a list of all dependencies
+  // These could either be ordinary dependencies or shaper plugins
   const dependencyNames = new Set([
     ...Object.keys(packageJson.dependencies || {}),
     ...Object.keys(packageJson.devDependencies || {}),
   ]);
-
   const allDependencyNames = Array.from(dependencyNames);
-  const plugins = new Map<string, Plugin>();
 
-  // loop through all the dependencies
+  // Loop through all dependencies and build a map of shaper plugins
+  const plugins = new Map<string, Plugin>();
   allDependencyNames.forEach((dependencyName) => {
-    // Get the package.json file
-    const dependencyPackageJson = getPluginPackageJson(
+    // Get the package.json file of the dependency
+    const dependencyPackageJson = getDependencyPackageJson(
       rootPath,
       dependencyName
     );
 
-    // check to see that if has the required properties
+    // Check to see if required properties exist
     if (
       dependencyPackageJson &&
-      !!dependencyPackageJson.shaper &&
-      !!dependencyPackageJson.main
+      !!dependencyPackageJson.main &&
+      !!dependencyPackageJson.shaper
     ) {
-      // get the plugin
+      // It is a shaper plugin, get it!
       const plugin = getPluginMainDefaultExport(
         path.join(rootPath, 'node_modules', dependencyName),
         dependencyPackageJson.main
@@ -50,6 +61,39 @@ function getInstalledPluginsFromPackageJson(
   return plugins;
 }
 
+/**
+ * Returns the package.json file of the specified dependency
+ *
+ * @param rootPath e.g. ~/projects/code-shaper
+ * @param dependencyName e.g. @code-shaper/react
+ */
+function getDependencyPackageJson(
+  rootPath: string,
+  dependencyName: string
+): PackageJson | null {
+  try {
+    const packageJsonPath = require.resolve(`${dependencyName}/package.json`, {
+      paths: [rootPath],
+    });
+    return JsonUtils.readJsonFile<PackageJson>(packageJsonPath);
+  } catch {
+    return null;
+  }
+}
+
+/**
+ * Returns the main default export of the specified plugin
+ *
+ * @param rootPath e.g. ~/projects/code-shaper/node_modules/@code-shaper/react
+ * @param mainPath e.g. ./dist/index.js
+ * @returns the main default export of the plugin, e.g.
+ * {
+ *   id: '@code-shaper/react',
+ *   name: 'React',
+ *   description: 'generates React applications',
+ *   run: [AsyncFunction: run]
+ * }
+ */
 function getPluginMainDefaultExport(
   rootPath: string,
   mainPath: string
@@ -61,20 +105,6 @@ function getPluginMainDefaultExport(
     return require(fullMainPath).default as Plugin;
   } catch (err) {
     console.log('error requiring plugin', err);
-    return null;
-  }
-}
-
-function getPluginPackageJson(
-  workspaceRoot: string,
-  pluginName: string
-): PackageJson | null {
-  try {
-    const packageJsonPath = require.resolve(`${pluginName}/package.json`, {
-      paths: [workspaceRoot],
-    });
-    return JsonUtils.readJsonFile<PackageJson>(packageJsonPath);
-  } catch {
     return null;
   }
 }
