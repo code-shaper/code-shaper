ccb695a6be05a35aa7762d5fda34c9ac1e0f8e14
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-06-06 03:16:30 -0400

    Added JSDoc comments for Options

--- packages/shaper-utils/src/models/Options.ts
+++ packages/shaper-utils/src/models/Options.ts
@@ -1,1 +1,12 @@
+/**
+ * Options is a hash of key-value pairs, where keys are strings and values
+ * can be anything (usually strings, arrays or objects).
+ *
+ * Example:
+ * {
+ *   "itemName": "Button",
+ *   "workspace": "packages/ui-lib",
+ *   "dirInWorkspace": "src/components/Button",
+ * }
+ */
 export type Options = { [option: string]: unknown };
