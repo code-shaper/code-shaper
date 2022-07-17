7892b14813d684de51eea4e126855b3055c6d949
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-07-17 18:33:43 -0400

    Added tests for typescript plugin

---
+++ plugins/typescript/src/typescriptLibraryGenerator/expected-output/packages/string-utils/README.md
@@ -0,0 +1,4 @@
+# String Utils
+
+This is a [TypeScript](https://typescriptlang.org/) library bootstrapped with
+[Code Shaper](https://code-shaper.dev).
--- plugins/typescript/src/typescriptLibraryGenerator/typescriptLibraryGenerator.test.ts
+++ plugins/typescript/src/typescriptLibraryGenerator/typescriptLibraryGenerator.test.ts
@@ -1,4 +1,6 @@
-// import plugin from '../index';
+import path from 'path';
+import { FileUtils } from '@code-shaper/shaper-utils';
+import { typescriptLibraryGenerator } from './index';
 
 describe('typescriptLibraryGenerator', () => {
   test('should create a new TypeScript library from templates', async () => {
@@ -13,8 +15,21 @@ describe('typescriptLibraryGenerator', () => {
     //   packageName: '@movie-magic/string-utils',
     // });
 
-    // TODO: Compare test-output with expected-output
-    expect(true).toBeTruthy();
+    // Delete test-output if it exists
+    const testOutput = path.join(__dirname, 'test-output');
+    FileUtils.deletePath(testOutput);
+
+    // Run the generator
+    await typescriptLibraryGenerator.generate(testOutput, {
+      itemName: 'string-utils',
+      parentDir: path.join(testOutput, 'packages'),
+      packageName: '@movie-magic/string-utils',
+    });
+
+    // Compare test-output with expected-output
+    const expectedOutput = path.join(__dirname, 'expected-output');
+    const result = FileUtils.compareDirectories(expectedOutput, testOutput);
+    expect(result.same).toBe(true);
 
     // restore console logs
     jest.restoreAllMocks();
