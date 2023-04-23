ee92607262ca99d2b3320e4cb064bef5f27b6c9e
Naresh Bhatia <me@nareshbhatia.dev>
2023-04-23 14:14:36 -0400

    Updated dependencies

--- plugins/plugin/jest.config.js
+++ plugins/plugin/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'jest-config-custom',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['expected-output', 'test-output'],
+  modulePathIgnorePatterns: ['expected-output', 'test-output'],
 };
