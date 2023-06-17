3f2b9f1476315308c34d5efd1d48796f321671b6
Naresh Bhatia <me@nareshbhatia.dev>
2023-06-17 18:49:11 -0400

    Added Header stories

---
+++ plugins/react/src/appGenerator/templates/src/components/Header/Header.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Header } from './Header';
+
+const meta = {
+  title: 'Components/Header',
+  component: Header,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic = {} satisfies Story;
