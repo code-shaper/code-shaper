08eec54ad42048158bedad7b70fa9e57d530f0b7
Naresh Bhatia <me@nareshbhatia.dev>
2023-06-26 15:59:35 -0400

    fix: fix eslint error in msw mock handler

--- plugins/react/src/appGenerator/templates/src/mocks/handlers.ts
+++ plugins/react/src/appGenerator/templates/src/mocks/handlers.ts
@@ -2,7 +2,7 @@ import { MOCK_API_URL } from './constants';
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get(`${MOCK_API_URL}/products`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json([]));
-  }),
+  rest.get(`${MOCK_API_URL}/products`, async (req, res, ctx) =>
+    res(ctx.status(200), ctx.json([]))
+  ),
 ];
