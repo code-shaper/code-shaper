eec24463ab48cecbc775fa4bf4ce5e02cc20e2da
Naresh Bhatia <me@nareshbhatia.dev>
2023-09-05 21:46:46 -0400

    chore: add module-name-mapper to jest-config

--- plugins/repo/src/turborepoGenerator/expected-output/configs/jest-config-custom/jest-preset.js
+++ plugins/repo/src/turborepoGenerator/expected-output/configs/jest-config-custom/jest-preset.js
@@ -6,6 +6,12 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'jest-config-custom/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'jest-config-custom/__mocks__/styleMock.js',
+  },
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
