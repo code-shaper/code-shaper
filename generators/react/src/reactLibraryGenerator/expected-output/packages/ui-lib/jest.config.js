f151671eb5ad8d1c20fabaa4e4cb59958ee80dea
Naresh Bhatia <me@nareshbhatia.dev>
2024-09-29 13:50:49 -0400

    chore: updated jest.config.js generator to set coverage thresholds as appropriate

--- generators/react/src/reactLibraryGenerator/expected-output/packages/ui-lib/jest.config.js
+++ generators/react/src/reactLibraryGenerator/expected-output/packages/ui-lib/jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: '@code-shaper/jest-config',
   testEnvironment: 'jsdom',
+
+  // Set coverage threshold as appropriate
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
 };
